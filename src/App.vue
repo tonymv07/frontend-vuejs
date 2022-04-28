297c6df34004ca2be7406fc2a1acac9f40d22fb8
Tony Mora <tonyajmv07@gmail.com>
2022-04-27 23:55:54 -0600

    limpia_plantilla

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+
+
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  </header>
+</template>
+
+<style>
+@import './assets/base.css';
+
+#app {
+  max-width: 1280px;
+  margin: 0 auto;
+  padding: 2rem;
+
+  font-weight: normal;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+
+
+</style>
